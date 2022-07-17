5129745703b2c080a02b6d5ad30ed2a34cf43b06
ashrasmun <ashrasmun@gmail.com>
2022-07-17 12:42:27 +0200

    Add startup script
    This script needs to be put to \`Win\` -> \`Run\` -> \`shell:startup\` or simply \`C:\Users\ashra\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\`. After that it will automatically switch escape with capslock on startup

---
+++ capslock_to_esc/startup.ps1
@@ -0,0 +1,3 @@
+# Before running this script, make sure you can do it :)
+# Set-ExecutionPolicy -ExecutionPolicy Bypass -Scope CurrentUser -Force
+Start-Process "c:\home\repos\ahk_win_utils\capslock_to_esc\capslock_to_esc.ahk" -verb runAs
