37986b93116b545dc748563e5883b9b06625ea6a
ashrasmun <ashrasmun@gmail.com>
2022-10-03 10:09:04 +0200

    Require .exe next to the .ps1

--- capslock_to_esc/startup.ps1
+++ capslock_to_esc/startup.ps1
@@ -1,3 +1,4 @@
 # Before running this script, make sure you can do it :)
 # Set-ExecutionPolicy -ExecutionPolicy Bypass -Scope CurrentUser -Force
-Start-Process "c:\home\repos\ahk_win_utils\capslock_to_esc\capslock_to_esc.ahk" -verb runAs
+$ScriptPath = -Join($PSScriptRoot, "capslock_to_esc.exe" )
+Start-Process $ScriptPath -Verb runAs
