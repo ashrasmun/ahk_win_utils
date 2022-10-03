0cb56c4b6b869b6d496a64ed042e26f61d58df75
ashrasmun <ashrasmun@gmail.com>
2022-10-03 10:14:41 +0200

    Add startup script and fix the path creation

---
+++ capslock_to_esc/startup.bat
@@ -0,0 +1,1 @@
+@powershell -File startup.ps1
--- capslock_to_esc/startup.ps1
+++ capslock_to_esc/startup.ps1
@@ -1,4 +1,10 @@
 # Before running this script, make sure you can do it :)
 # Set-ExecutionPolicy -ExecutionPolicy Bypass -Scope CurrentUser -Force
-$ScriptPath = -Join($PSScriptRoot, "capslock_to_esc.exe" )
+$ScriptPath = -Join($PSScriptRoot, "\\capslock_to_esc.exe")
+
+If (-Not (Test-Path -Path $ScriptPath)) {
+    Write-Host Expected $ScriptPath to be compiled.
+    Exit
+}
+
 Start-Process $ScriptPath -Verb runAs
